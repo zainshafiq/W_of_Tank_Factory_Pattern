e868602d91f439ffdfb6910305586bb143580dd7
Shafiq Zainuddin <zainshafiq18@yahoo.com>
2019-03-04 20:21:51 -0600

    Tank Commit

---
+++ dreiTank.java
@@ -0,0 +1,17 @@
+
+package ENSE470_Lab1;
+
+public class dreiTank extends Tank {
+
+    public dreiTank(String name, String country, int hitPoints, int armor, $
+            setName(name);
+            setCountry(country);
+            setHitPoints(hitPoints);
+            setArmor(armor);
+            setShellDamage(shellDamage);
+            setCurrentHealth(currentHealth);
+
+            System.out.println(name + " Tank created");
+            System.out.println("This tank is from " + country + "\n");
+    }
+}
---
+++ einsTank.java
@@ -0,0 +1,17 @@
+
+package ENSE470_Lab1;
+
+public class einsTank extends Tank {
+
+    public einsTank(String name, String country, int hitPoints, int armor, $
+            setName(name);
+            setCountry(country);
+            setHitPoints(hitPoints);
+            setArmor(armor);
+            setShellDamage(shellDamage);
+            setCurrentHealth(currentHealth);
+
+            System.out.println(name + " Tank created");
+            System.out.println("This tank is from " + country + "\n");
+    }
+}
